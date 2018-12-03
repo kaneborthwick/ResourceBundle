30b5c970006f6c314bcf30470c55d7ab450c0772
Kane Borthwick <kane.borthwick@hotmail.com>
2018-12-03 11:06:16 +1100

    Update doctrine.config.php

--- src/config/app/doctrine.config.php
+++ src/config/app/doctrine.config.php
@@ -1,33 +1,23 @@
 <?php
-namespace TowerResourceBundle;
-
-use Gedmo\Loggable\Entity;
 
 return [
-    'doctrine' => [
-        'event_manager' => [
-            'orm_default' => [
-                'subscribers' => [
-                    'Gedmo\Timestampable\TimestampableListener',
-                    'Gedmo\SoftDeleteable\SoftDeleteableListener',
-                    'ORMMappedSuperClassSubscriber',
-                    'Gedmo\Sortable\SortableListener',
-                ],
-            ],
-        ],
-        'driver' => [
-            'orm_default' => [
-                'drivers' => [
-                    Entity::class => 'resource_bundle_config',
-                ],
-            ],
-        ],
-        'configuration' => [
-            'orm_default' => [
-                'filters' => [
-                    'soft-deleteable' => 'Gedmo\SoftDeleteable\Filter\SoftDeleteableFilter',
-                ],
-            ],
-        ],
-    ],
+	'doctrine' => [
+		'event_manager' => [
+			'orm_default' => [
+				'subscribers' => [
+					'Gedmo\Timestampable\TimestampableListener',
+					'Gedmo\SoftDeleteable\SoftDeleteableListener',
+					'ORMMappedSuperClassSubscriber',
+					'Gedmo\Sortable\SortableListener',
+				],
+			],
+		],
+		'configuration' => [
+			'orm_default' => [
+				'filters' => [
+					'soft-deleteable' => 'Gedmo\SoftDeleteable\Filter\SoftDeleteableFilter',
+				],
+			],
+		],
+	],
 ];
